aed020a616b3c388365eea997305627676df9773
pbrearton94 <pbrearton@gmail.com>
2023-05-31 13:03:34 -0400

    Added state utils - setup the initial state, the various states of the shape and the default grid.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import './App.css';
-import Controls from './components/controls/controls.component';
-import GridBoard from './components/grid-board/grid-board.component';
-import MessagePopup from './components/message-popup/message-popup.component';
-import NextBlock from './components/next-block/next-block.component';
-import ScoreBoard from './components/score-board/score-board.component';
+import "./App.css";
+import {Provider} from 'react-redux';
+import { createStore } from "redux";
+import reducers from "./reducers";
+import Controls from "./components/controls/controls.component";
+import GridBoard from "./components/grid-board/grid-board.component";
+import MessagePopup from "./components/message-popup/message-popup.component";
+import NextBlock from "./components/next-block/next-block.component";
+import ScoreBoard from "./components/score-board/score-board.component";
+
+const store = createStore(reducers);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Tetris</h1>
-      </header>
-      <GridBoard />
-      <NextBlock />
-      <ScoreBoard />
-      <Controls />
-      <MessagePopup />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <h1>Tetris</h1>
+        </header>
+        <GridBoard />
+        <NextBlock />
+        <ScoreBoard />
+        <Controls />
+        <MessagePopup />
+      </div>
+    </Provider>
   );
 }
 
