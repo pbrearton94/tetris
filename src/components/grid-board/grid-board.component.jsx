2e4217ca26c4b8932c913bdc91d563585050ce9a
pbrearton94 <pbrearton@gmail.com>
2023-05-31 13:48:20 -0400

    added connection to grid board

--- src/components/grid-board/grid-board.component.jsx
+++ src/components/grid-board/grid-board.component.jsx
@@ -1,21 +1,39 @@
 import React from "react";
 import GridSquare from "../grid-square/grid-square.component";
-import './grid-board.style.scss';
+import { useSelector } from "react-redux";
+import "./grid-board.style.scss";
+import { shapes } from "../../utils";
 
 /* Create a 10 x 18 grid */
 const GridBoard = (props) => {
-  const grid = [];
+  //const grid = [];
+  const game = useSelector((state) => state.game);
+  const { grid, shape, rotation, x, y, isRunning, speed } = game;
+  const block = shapes[shape][rotation];
+  const blockColor = shape;
+  const gridSquares = grid.map((rowArray, row) => {
+    return rowArray.map((square, col) => {
+      const blockX = col - x;
+      const blockY = row - y;
+      let color = square;
 
-  for (let row = 0; row < 18; row++) {
-    grid.push([]);
-    for (let col = 0; col < 10; col++) {
-      grid[row].push(<GridSquare key={`${col}${row}`} color="1" />);
-    }
-  }
+      if (
+        blockX >= 0 &&
+        blockX < block.length &&
+        blockY >= 0 &&
+        blockY < block.length
+      ) {
+        color = block[blockY][blockX] === 0 ? color : blockColor;
+      }
+
+      const k = row * grid[0].length + col;
+      return <GridSquare key={k} color={color} />;
+    });
+  });
 
   return (
     <>
-      <div className="grid-board">{grid}</div>
+      <div className="grid-board">{gridSquares}</div>
     </>
   );
 };
