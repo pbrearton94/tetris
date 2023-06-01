a4845d292b0454f8d434b880ea79c1efcb3b8402
pbrearton94 <pbrearton@gmail.com>
2023-06-01 09:44:57 -0400

    timer created.

--- src/components/grid-board/grid-board.component.jsx
+++ src/components/grid-board/grid-board.component.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import GridSquare from "../grid-square/grid-square.component";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./grid-board.style.scss";
 import { shapes } from "../../utils";
+import { moveDown } from "../../actions";
 
 /* Create a 10 x 18 grid */
 const GridBoard = (props) => {
   //const grid = [];
+  const requestRef = useRef();
+  const lastUpdateTimeRef = useRef(0);
+  const progressTimeRef = useRef(0);
+  const dispatch = useDispatch();
   const game = useSelector((state) => state.game);
   const { grid, shape, rotation, x, y, isRunning, speed } = game;
   const block = shapes[shape][rotation];
   const blockColor = shape;
+  
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(update);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, [isRunning]);
+
+  
   const gridSquares = grid.map((rowArray, row) => {
     return rowArray.map((square, col) => {
       const blockX = col - x;
@@ -30,6 +42,23 @@ const GridBoard = (props) => {
       return <GridSquare key={k} color={color} />;
     });
   });
+
+  const update = (time) => {
+    requestRef.current = requestAnimationFrame(update);
+    if (!isRunning) {
+      return;
+    }
+    if (!lastUpdateTimeRef.current) {
+      lastUpdateTimeRef.current = time;
+    }
+    const deltaTime = time - lastUpdateTimeRef.current;
+    progressTimeRef.current += deltaTime;
+    if (progressTimeRef.current > speed) {
+      dispatch(moveDown());
+      progressTimeRef.current = 0;
+    }
+    lastUpdateTimeRef.current = time;
+  };
 
   return (
     <>
